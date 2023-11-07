d9ca47a05dc2f5220539787e9cdf587d93f24554
Alia Khizer <aliakhizar6@gmail.com>
2023-11-07 10:37:04 +0500

    create page

--- javascript.js
+++ javascript.js
@@ -1,12 +1,3 @@
-// function getRating(ratingValue) {
-//   console.log(ratingValue);
-//   if (i <= ratingValue) {
-//     return `<img src="./images/Star1.svg" alt="Star" />`;
-//   } else {
-//     return `<img src="./images/Star5.svg" alt="Star" />`;
-//   }
-// }
-
 function getRating(ratingValue) {
   let stars = "";
   for (let i = 1; i <= 5; i++) {
@@ -19,6 +10,58 @@ function getRating(ratingValue) {
   return stars;
 }
 
+let result;
+
+function search() {
+  const input = document.getElementById("search-input").value.toLowerCase();
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(input)
+  );
+
+  const container = document.querySelector(".gallery-grid");
+  container.innerHTML = "";
+
+  filteredProducts.forEach((product) => {
+    const { title, price, image, rating } = product;
+    const ratingValue = Math.round(rating.rate);
+    const stars = getRating(ratingValue);
+
+    container.innerHTML += `
+                <div class="gallery-item">
+                    <div class="gallery-image">
+                        <img src="${image}" alt="${title}" />
+                    </div>
+                    <div class="phone-info">
+                        <div class="phone-details">
+                            <div class="phone-title">${title}</div>
+                            <div class="phone-price">$${price}</div>
+                        </div>
+                        <div class="phone-actions">
+                            <div class="star-ratings">${stars}</div>
+                            <button class="add-to-cart">Add to Cart</button>
+                        </div>
+                        <div class="off-percent">
+                            <div>56%</div>
+                            <div>OFF</div>
+                        </div>
+                    </div>
+                </div>
+                `;
+  });
+}
+
+let count = 0;
+const counter = function () {
+  count++;
+  console.log(count);
+};
+
+let products;
+
+const button = document.querySelector(".add-to-cart");
+button.addEventListener("click", counter);
+
 const container = document.querySelector(".gallery-grid");
 
 fetch("https://fakestoreapi.com/products")
@@ -26,35 +69,37 @@ fetch("https://fakestoreapi.com/products")
     return response.json();
   })
   .then((data) => {
-    let products = data;
+    products = data;
+
     products.map((product) => {
+
+
+     
+
       const { title, price, image, rating } = product;
 
       const ratingValue = Math.round(rating.rate);
-      console.log(ratingValue);
       const stars = getRating(ratingValue);
 
       return (container.innerHTML += `
-      <div class="gallery-item">
-          <div class="gallery-image">
-            <img src="${image}" alt="${title}" />
-          </div>
-          
-          <div class="phone-info">
-            <div class="phone-details">
-              <div class="phone-title">${title}</div>
-              <div class="phone-price">$${price}</div>
-            </div>
-            <div class="phone-actions">
-              <div class="star-ratings">${stars}</div>
-              <button class="add-to-cart">Add to Cart</button>
-            </div>
-            <div class="off-percent">
-              <div>56%</div>
-              <div>OFF</div>
-            </div>
-        
-        </div>
-        `);
+                    <div class="gallery-item">
+                        <div class="gallery-image">
+                            <img src="${image}" alt="${title}" />
+                        </div>
+                        <div class="phone-info">
+                            <div class="phone-details">
+                                <div class="phone-title">${title}</div>
+                                <div class="phone-price">$${price}</div>
+                            </div>
+                            <div class="phone-actions">
+                                <div class="star-ratings">${stars}</div>
+                                <button class="add-to-cart">Add to Cart</button>
+                            </div>
+                            <div class="off-percent">
+                                <div>56%</div>
+                                <div>OFF</div>
+                            </div>
+                        </div>
+                    `);
     });
   });
